a29cf919d8d666a3383ac1c13fb0b33a3bad5d7d
VarunG <Hi@Varun20-Desk>
2025-11-24 19:36:37 +0100

    new changes made

--- main.py/main.py
+++ main.py/main.py
@@ -6,3 +6,6 @@
 #the code seems wrong
 def use_ai_generated code():
   name= 123;
+  
+#create a new   file i will noy use this 
+
---
+++ main.py/second.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# i think this is correct
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    return data
