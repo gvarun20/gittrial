d80b86d3a6d236f7f3e7f6ed6991efd1f8522261
Varun Gurupurandar <86005809+gvarun20@users.noreply.github.com>
2025-11-24 19:38:00 +0100

    correction and work onto new task

--- main.py/second.py
+++ main.py/second.py
@@ -4,3 +4,7 @@ import pandas as pd
 def load_data(file_path):
     data = pd.read_csv(file_path)
     return data
+
+
+##this seems correct , work on other task as well 
+# good job on learning this
